1539ee16216e618acd578dac46347779091fd3e7
at15 <at15@outlook.com>
2013-11-23 22:15:31 +0800

    memeory leak
    why the processbig dont have, but the CvPicCtrl has

--- TrackTheCar/CvPicCtrl.cpp
+++ TrackTheCar/CvPicCtrl.cpp
@@ -12,13 +12,40 @@ CCvPicCtrl::~CCvPicCtrl(void)
 }
 
 /*virtual*/ bool CCvPicCtrl::Init(CWnd*dlg_hwnd,int nID){
+    // get hwnd for the pic control 
+    pic_CWnd = dlg_hwnd->GetDlgItem(nID);//GetDlgItem(hdlg,nID); 
+    if(!pic_CWnd) return false;
+
+    // get the rect( size and position ) of the pic control
+    pic_CWnd->GetClientRect(&pic_rect);
+
+    // get the CDC of the pic control
+    pic_CDC = pic_CWnd->GetDC();  
+    // get the HDC from CDC
+    pic_hDC = pic_CDC->GetSafeHdc();
+    if(!pic_hDC) return false;
+
     return true;
 }
 
-/*virtual*/ bool CCvPicCtrl::SetCurrentFrame(IplImage* new_frame){
-    return true;
+/*virtual*/ IplImage* CCvPicCtrl::SetCurrentFrame(IplImage* new_frame){
+    CvvImage m_CvvImage;  
+    m_CvvImage.CopyOf(new_frame,1);     
+    m_CvvImage.DrawToHDC(pic_hDC, &pic_rect);
+    IplImage* t = m_current_frame;
+    m_current_frame = new_frame;
+    return t;
 }
 
 /*virtual*/IplImage* CCvPicCtrl::GetCurrentFrame(){
-    return NULL;
+    return m_current_frame;
+}
+
+void CCvPicCtrl::ShowBlack(){
+    // make the pic_control black
+    CDC MemDC;
+    MemDC.CreateCompatibleDC(NULL);  
+    MemDC.FillSolidRect(pic_rect,RGB(0,0,0)); 
+    HDC tDC = MemDC.GetSafeHdc();
+    pic_CDC->StretchBlt(pic_rect.left,pic_rect.top,pic_rect.Width(),pic_rect.Height(),&MemDC,0,0,48,48,SRCCOPY);
 }
--- TrackTheCar/CvPicCtrl.h
+++ TrackTheCar/CvPicCtrl.h
@@ -10,7 +10,26 @@ public:
 
 public:
     virtual bool Init(CWnd*dlg_hwnd,int nID);
-    virtual bool SetCurrentFrame(IplImage* new_frame);
+    virtual IplImage* SetCurrentFrame(IplImage* new_frame);
     virtual IplImage* GetCurrentFrame();
+    void ShowBlack();
+private:
+    IplImage* m_current_frame;
+
+    /*=========================================================*/
+    /* the handles for the pic control */
+    // The CWnd for the dlg, get its HWND by pic_CWnd->m_hwnd
+    CWnd* pic_CWnd;
+    // The rect of the pic control
+    // TODO:note: there seems to be differences between the axis in opencv and Windows
+    CRect pic_rect; 
+    // The HDC will be used by CvvImage to show OpenCV image to the pic control
+    // See MyCvvImage.h .cpp to know more... ( seems to be about fill a structure and 
+    // stretch if needed )
+    HDC pic_hDC;  
+    // The CDC is the wrapper for HDC by MFC, use it to manager easier
+    // Although it is only used in
+    // Init and ShowDefault
+    CDC* pic_CDC;
 };
 
