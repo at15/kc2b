eed9749c9fec41c14dc0ffc4f91134c5d1f0f104
at15 <at15@outlook.com>
2013-12-06 22:18:50 +0800

    now we can get car pos technically

---
+++ TrackTheCar/SmallCar.cpp
@@ -0,0 +1,44 @@
+#include "StdAfx.h"
+#include "SmallCar.h"
+
+
+CSmallCar::CSmallCar(void)
+{
+}
+
+
+CSmallCar::~CSmallCar(void)
+{
+}
+
+// we need 
+// 1 a picture control that can always give us the map
+//   PS: we only need a processed image now
+// 2 the camera that can get the latest image, so we can know where the car is
+bool CSmallCar::Init(CCvPicCtrl* camera,IplImage* map,CConfigs* config){
+    m_camera = camera;
+    m_map.SetImage(map);
+    m_config = config;
+}
+
+bool CSmallCar::GetCarPos(){
+    IplImage* t = m_camera->GetCurrentFrame();
+    m_head = m_proc.GetRedCore(t,m_config->GetThreshold());
+    m_tail = m_proc.GetBlueCore(t,m_config->GetThreshold());
+}
+
+void CSmallCar::GoLeft(){
+
+}
+
+void CSmallCar::GoRight(){
+
+}
+
+void CSmallCar::GoBack(){
+
+}
+
+void CSmallCar::GoForward(){
+
+}
---
+++ TrackTheCar/SmallCar.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "CameraCtrl.h"
+#include "Configs.h"
+#include "TempImage.h"
+
+class CSmallCar
+{
+public:
+    CSmallCar(void);
+    ~CSmallCar(void);
+public:
+    bool Init(CCvPicCtrl* camera,IplImage* map,CConfigs* config);
+
+    // control the small car
+    void GoLeft();
+    void GoRight();
+    void GoBack();
+    void GoForward();
+private:
+    CImageProc m_proc;
+
+    CCvPicCtrl* m_camera;
+    CConfigs* m_config;
+    CTempImage m_map;
+
+    CvPoint m_head;
+    CvPoint m_tail;
+
+    bool GetCarPos();
+};
+
